5d3fbe9b1bc6b73e2fe3c055de5ecff64a3b5aaa
nvengal <nvengal@boldpenguin.com>
2020-04-10 15:02:46 -0400

    Add setup to fix longstanding docker mysql issue

--- setup-debian.sh
+++ setup-debian.sh
@@ -22,4 +22,15 @@ sudo apt install fonts-hack-ttf -y
 [ -n "$(command -v starship)" ] \
   || curl -fsSL https://starship.rs/install.sh | bash
 
+# Docker mysql conf
+#   Unclear why this step is necessary
+#   Without it mysql in docker is not allowed to create a lock file
+#   In theory mysql will run with user id 999 in the container
+#     https://github.com/docker-library/mysql/blob/d284e15821ac64b6eda1b146775bf4b6f4844077/8.0/Dockerfile#L3
+mysql_conf_file="/usr/lib/tmpfiles.d/mysqld.conf"
+mysql_conf_command="d /var/run/mysqld 0755 999 999 -"
+if [ ! -f $mysql_conf_file ]; then
+    echo $mysql_conf_command | sudo tee $mysql_conf_file
+fi
+
 echo "SUCCESS"
