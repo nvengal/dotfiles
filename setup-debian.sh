ce35ff79f581865aa67eceb0b846ce759deaa0b2
nvengal <nikhilvengal@gmail.com>
2020-04-09 20:39:58 -0400

    Add tmux to debian setup

--- setup-debian.sh
+++ setup-debian.sh
@@ -4,10 +4,15 @@ set -e
 
 sudo apt update
 
+# TMUX
+#   https://github.com/tmux/tmux
+sudo apt install tmux -y
+
 # Powerline Hack Font (used by Starship)
 #   https://github.com/source-foundry/Hack
-sudo apt install fonts-hack-ttf
+sudo apt install fonts-hack-ttf -y
 
 # Starship Prompt
 #   https://starship.rs/
-curl -fsSL https://starship.rs/install.sh | bash
+[ -n "$(command -v starship)" ] \
+  || curl -fsSL https://starship.rs/install.sh | bash
