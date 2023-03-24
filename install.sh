a7f13ee5f8496eedbf95290af5aa3d69d16b180f
Nikhil Vengal <ngvengal@gmail.com>
2023-03-23 18:25:33 -0700

    Install powerline fonts

--- install.sh
+++ install.sh
@@ -12,33 +12,45 @@ setup_vim() {
   fi
 }
 
+install_fonts() {
+  # Powerline Fonts
+  # https://github.com/powerline/fonts
+  git clone https://github.com/powerline/fonts.git --depth=1
+  pushd fonts && ./install.sh
+  popd && rm -rf fonts
+}
+
 ## tmux
 #ln -is ${PWD}/tmux.conf ${HOME}/.tmux.conf
-#
-## asdf version manager https://github.com/asdf-vm/asdf
-#if [ ! -d "${HOME}/.asdf" ]
-#then
-#  git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
-#  pushd $HOME/.asdf
-#  git checkout "$(git describe --abbrev=0 --tags)"
-#  popd
-#
-#  ln -is ${PWD}/asdfrc ${HOME}/.asdfrc
-#
-#  ./install-asdf-plugins.sh
-#fi
-#
-## Powerline Fonts
-## https://github.com/powerline/fonts
-#git clone https://github.com/powerline/fonts.git --depth=1
-#pushd fonts && ./install.sh
-#popd && rm -rf fonts
+
+# # asdf version manager https://github.com/asdf-vm/asdf
+# install_asdf() {
+#   if [ ! -d "${HOME}/.asdf" ]
+#   then
+#     git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
+#     pushd $HOME/.asdf
+#     git checkout "$(git describe --abbrev=0 --tags)"
+#     popd
+
+#     install_asdf_plugins
+#   fi
+# }
+
+# install_asdf_plugins() {
+#   readonly STARSHIP_VERSION=1.4.1
+
+#   echo "Installing starship ${STARSHIP_VERSION}"
+#   asdf plugin add starship && \
+#     asdf install starship $STARSHIP_VERSION && \
+#     asdf global starship $STARSHIP_VERSION
+# }
 
 install_linux() {
   packages="build-essential curl file git libssl-dev stow xclip"
   sudo apt update && sudo apt install --assume-yes $packages
-  stow git vim
+  stow asdf git vim
   setup_vim
+  install_fonts
 }
 
 main() {
