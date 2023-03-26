6a9cdf5cb4be298a29c56b6ce37a616892d197c2
Nikhil Vengal <ngvengal@gmail.com>
2023-03-26 00:40:13 -0700

    Revert "Add mariana colorscheme to zellij"
    This reverts commit 3594e8de6b6dbf24acf2f27a9fbd4f3d886956c0.
    Couldn't get highlight/selection colors to work

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ install_docker() {
 install_linux() {
   packages="alacritty build-essential cmake curl file git libssl-dev stow tree uidmap unzip vim xclip"
   sudo apt update && sudo apt install --assume-yes $packages
-  stow alacritty bash git vim zellij
+  stow alacritty bash git vim
   setup_vim
   install_fonts
   install_rust
