03473d05f3dcb245cc083b339df5ef82e663d3f7
Nikhil Vengal <ngvengal@gmail.com>
2023-11-21 10:21:37 -0800

    Use gitlinker to open highlighted code in github

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -3,6 +3,6 @@ vim.g.git_messenger_always_into_popup = true
 return {
   -- https://github.com/rhysd/git-messenger.vim
   { "rhysd/git-messenger.vim" },
-  -- https://github.com/Almo7aya/openingh.nvim
-  { "almo7aya/openingh.nvim" },
+  -- https://github.com/ruifm/gitlinker.nvim
+  { "ruifm/gitlinker.nvim" }
 }
