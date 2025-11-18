b9286f042bd37c2f2406e49d55ff5f99ea3f86a1
Nikhil Vengal <ngvengal@gmail.com>
2025-11-18 10:47:49 -0800

    Remove old nvim configs

--- nvim/.config/nvim.bak/init.lua
+++
@@ -1,9 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- explicitly setup gitlinker to use required function
-require("gitlinker").setup({
-  opts = {
-    action_callback = require"gitlinker.actions".open_in_browser,
-  },
-})
