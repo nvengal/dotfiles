ab04d1de5d4fc3375b37612254908463e99f29b8
Nikhil Vengal <ngvengal@gmail.com>
2025-11-18 10:53:40 -0800

    Update to lazyvim 15 (#2)
    * Update lazyvim config files
    
    * Add juliana colorscheme
    
    * Don't autoformat files on save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- explicitly setup gitlinker to use required function
-require("gitlinker").setup({
-  opts = {
-    action_callback = require"gitlinker.actions".open_in_browser,
-  },
-})
--- nvim/.config/nvim/lua/plugins/git.lua
+++
@@ -1,8 +0,0 @@
-vim.g.git_messenger_always_into_popup = true
-
-return {
-  -- https://github.com/rhysd/git-messenger.vim
-  { "rhysd/git-messenger.vim" },
-  -- https://github.com/ruifm/gitlinker.nvim
-  { "ruifm/gitlinker.nvim" }
-}
